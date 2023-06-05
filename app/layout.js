c9bd94ee3fc0f8ec0777293b4e968b31b90f038e
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-06-05 18:38:21 -0400

    first commit

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import '@styles/globals.css'
+
+
+
+export const metadata = {
+  title: 'Prompt-AI',
+  description: 'Discover & Share AI Prompts',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+    <body>
+      
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          
+          {children}
+        </main>
+      
+    </body>
+  </html>
+  )
+}
---
+++ components/Feed.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed

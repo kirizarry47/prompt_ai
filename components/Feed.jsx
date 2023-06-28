ae47f8bc569a897c017fb61bad9d1b80a9a24e06
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-06-28 17:25:37 -0400

    Feed connects to user prompt collection in db need to style promptcard.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,75 @@
 "use client";
 
-const Feed = () => {
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>Feed</div>
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+
+  //Search States
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  //Fetches post stored in DB
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+   
+  
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+  return (
+       <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={allPosts} />
+
+      
+    </section>
   )
 }
 
